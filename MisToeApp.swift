87927d114a8c0b241f19f56a2b243b3e3842845f
aucodes <01astha@gmail.com>
2022-11-06 12:57:06 +0100

    Main App

---
+++ MisToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisToeApp.swift
+//  MisToe
+//
+//  Created by Astha Upadhyay on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MisToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
